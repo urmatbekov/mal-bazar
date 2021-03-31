6d9f7eab21238774a2ac24b975089bf944322c52
Nurkadyr <nurkadyrurmatbekov@gmail.com>
2021-03-31 17:18:55 +0600

    redux-form

--- src/reduce/index.js
+++ src/reduce/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import categories from "./categories";
 import products from "./products";
+import {reducer as form} from "react-redux"
 
 const reduce = combineReducers({
     categories,
-    products
+    products,
+    form
 })
 
 export default reduce;
